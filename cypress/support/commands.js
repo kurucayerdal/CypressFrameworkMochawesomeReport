8bb7167a88fd0dc880d65268ac2d84407d2648b6
Erdal KURUCAY <e.kurucay@evolucare.com>
2022-05-12 11:59:43 +0200

    first commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,39 @@
+
+  // for mochawesome report
+  import addContext from 'mochawesome/addContext';
+
+  Cypress.on('test:after:run', (test, runnable) =>{
+    if (test.state === "failed") {    
+      const screenshot =`assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;    
+      addContext({ test }, screenshot);  
+    }
+  });
+
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    cy.log(err.message);
+    return false;
+  });
+
+
+// ça marche mais une exeption
+// Cypress.Commands.add("addScreenshot", () =>
+// {
+//     const screenshotFile =`assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (screenshot)`; 
+//     const screenshot = cy.screenshot(screenshotFile);
+//     cy.once('test:after:run', (runnable, test)=>{
+//         addContext( {test} , screenshot);
+//     })
+// });
+
+
+
+// ça marche mais une exeption
+// Cypress.Commands.add("addScreenshot", () =>
+// {
+//     const screenshot = cy.screenshot('screenshot');
+//     addContext({test}, screenshot );
+// });
+
---
+++ cypress/support/pageObject/Apage.js
@@ -0,0 +1,34 @@
+const ReusableMethods = require("../utilities/ReusableMethods");
+
+const testElements =
+{
+    url : "https://www.google.com",
+    textOfTheElement : "J'accepte",
+    inputSearch : '[name="q"]',
+    searchValue : 'EVOLUCARE Technologies {enter}',
+    titleText : 'My Awesome Application'
+}
+
+class Apage extends ReusableMethods {
+
+    navigateToSite()
+    {
+        return cy.visit(testElements.url);
+    }
+
+    acceptCookies()
+    {
+        return cy.contains(testElements.textOfTheElement).click();
+    }
+
+    makeAsearch()
+    {
+        return this.typeFunction(testElements.inputSearch, testElements.searchValue);
+    }
+
+    verifyTheTitleOfThePage()
+    {
+        return this.verifyTitleOfThePage();
+    }
+}
+module.exports = Apage;
