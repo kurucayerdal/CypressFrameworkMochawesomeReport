905f59c96b68c5bce37cd7b373ab7eb7e7eac2cb
Erdal KURUCAY <e.kurucay@evolucare.com>
2022-05-16 15:47:28 +0200

    updated

--- cypress/integration/exampleTests/B/test2.spec.js
+++ cypress/integration/exampleTests/B/test2.spec.js
@@ -2,7 +2,7 @@
 describe("Test Suite 2",()=>{
 
 it("Test A", ()=>{
-cy.visit("https://www.google.com");
+cy.visit("/");
 cy.contains("J'accepte").click();
 cy.get('[name="q"]').type('EVOLUCARE Technologies {enter}');
 });
